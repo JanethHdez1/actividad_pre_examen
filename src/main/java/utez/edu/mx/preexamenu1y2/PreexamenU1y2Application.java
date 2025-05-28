ab1a38e48b06a319df55566902e1409abc84193f
JanethHdez1 <20223tn061@utez.edu.mx>
2025-05-28 17:54:09 -0600

    [FIRST_COMMIT] Subida inicial del proyecto

---
+++ src/main/java/utez/edu/mx/preexamenu1y2/PreexamenU1y2Application.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.preexamenu1y2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreexamenU1y2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreexamenU1y2Application.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/preexamenu1y2/PreexamenU1y2ApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.preexamenu1y2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreexamenU1y2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
